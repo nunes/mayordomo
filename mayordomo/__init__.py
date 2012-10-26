7c627e3386f00b44895f05c8b7a3a99bb3a68e28
nunes <nunes@es-robot.com>
2012-10-26 12:31:57 +0200

    changed image location

--- mayordomo/__init__.py
+++ mayordomo/__init__.py
@@ -1,41 +1,29 @@
 #!/usr/bin/env python
-
-#import win32gui
-#import time
-#import wx
-#
-#w=win32gui
-#time.sleep(1)
-#
-#print w.GetWindowText (w.GetForegroundWindow())
-#
-#app = wx.App(False)
-#frame = wx.Frame(None, wx.ID_ANY, "Hello World")
-#frame.Show(True)
-#app.MainLoop()
-
-
-import wx
-import win32gui
 import time
+import wx
 
-ID_ICON_TIMER = wx.NewId()
+try: import win32gui
+except ImportError:
+    pass
+
 
 class TaskBarFrame(wx.Frame):
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, style=wx.FRAME_NO_TASKBAR | wx.NO_FULL_REPAINT_ON_RESIZE)
 
         self.tbicon = wx.TaskBarIcon()
-        icon = wx.Icon('administrator.ico', wx.BITMAP_TYPE_ICO)
+        icon = wx.Icon('../images/administrator.ico', wx.BITMAP_TYPE_ICO)
         self.tbicon.SetIcon(icon, '')
         wx.EVT_TASKBAR_LEFT_DCLICK(self.tbicon, self.OnTaskBarLeftDClick)
         self.Show(True)
 
     def OnTaskBarLeftDClick(self, evt):
-        w=win32gui
         time.sleep(1)
-        print w.GetWindowText (w.GetForegroundWindow())
-        
+        print self.GetWindowName()
+
+    def GetWindowName(self):
+        return win32gui.GetWindowText (win32gui.GetForegroundWindow())
+
 app = wx.App(False)
 TaskBarFrame(None).Show(False)
 app.MainLoop()
